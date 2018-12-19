0272a9a340628eed6f7789a8f46cf9da5ef7be08
111 <2207601851@qq.com>
2018-12-19 16:58:22 +0800

    first commit

---
+++ Desktop/code/-1/XQDome/XQSmallNormal/app/src/main/java/android/small/indeedfortunate/xqsmallnormal/App.java
@@ -0,0 +1,25 @@
+package android.small.indeedfortunate.xqsmallnormal;
+
+import android.app.Application;
+import android.small.indeedfortunate.xqsmallnormal.util.ToastUtils;
+import android.small.indeedfortunate.xqsmallnormal.util.log.LogUtils;
+
+
+public class App extends Application {
+
+    private static App mApplication;
+
+    public static App getApplication() {
+        return mApplication;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mApplication = this;
+
+
+        ToastUtils.init(this);
+        LogUtils.init(true);
+    }
+}
